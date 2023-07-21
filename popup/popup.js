78967a5ad5625225fdc2a0d7257a44e7221585be
Joel Christopher <jchristo15@gmail.com>
2023-07-20 23:30:29 -0400

    Added event listener for start and pause click

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,19 @@
 console.log("this is a popup");
+
+
+// create variables to get id of buttons
+const start = document.querySelector("#start");
+const pause = document.querySelector("#pause");
+
+// initialize pause to be disabled
+pause.disabled = true;
+
+start.addEventListener("click", () => {
+  start.disabled = true;
+  pause.disabled = false;
+})
+
+pause.addEventListener("click", () => {
+  pause.disabled = true;
+  start.disabled = false;
+})
